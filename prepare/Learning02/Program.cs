88ac9ffbe13a5d8fa8117fbbc9760ae304a0ccf7
Stacy Yarrington <syarrington@enuit.com>
2024-11-12 22:29:05 -0700

    Learning02 assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Enuit";
+        job1._startYear = 2009;
+        job1._endYear = 2024;
+
+        // job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._jobTitle = "QA";
+        job2._company = "Nucleus";
+        job2._startYear = 2000;
+        job2._endYear = 2003;
+
+        // job2.DisplayJobDetails();
+
+        Resume resume = new Resume();
+        resume._name = "Stacy Yarrington";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        // Console.WriteLine(resume._jobs[0]._jobTitle);
+        resume.DisplayResume();
     }
 }
